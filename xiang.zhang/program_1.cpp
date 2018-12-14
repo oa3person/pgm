e5c9824ca095f62a2b448e7b00c83d42df7f4fee
oa3person <17610632689@163.com>
2018-12-14 10:20:06 +0800

    feat program_1.txt

---
+++ xiang.zhang/program_1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int MaxSubIncArr(int *arr, int length){
+    int maxSubLen = 1;
+    int index_s=0, index_e=1;
+    for(int index_e=1; index_e<length; index_e++){
+        if(arr[index_e-1] < arr[index_e]){
+            if(maxSubLen < (index_e-index_s+1)){
+                maxSubLen = index_e-index_s+1;
+            }
+        }else{
+            index_s = index_e;
+        }
+    }
+    return maxSubLen;
+}
+
+int main(void){
+    int arr[30];
+    int maxSubLen;
+    int lens;
+    cout<<"insert arr len"<<endl;
+    cin>>lens;
+    for(int i=0; i<lens; i++){
+        cin>>arr[i];
+    }
+    maxSubLen = MaxSubIncArr(arr, lens);
+    cout<<maxSubLen<<endl;
+    return 0;
+}
